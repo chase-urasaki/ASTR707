df05296d69f952a913835f71bf6cb54c621c9a10
chase-urasaki <chasemu@hawaii.edu>
2025-01-23 22:16:28 -1000

    reorganize

---
+++ module_1/module_1_proj.py
@@ -0,0 +1,9 @@
+#%%
+import numpy as np 
+
+# %%
+exec(open('check_sector.py').read())
+# %%
+import module_1.plot_tess as plot_tess
+import module_1.check_sector as check_sector
+# %%
--- module_1_proj.py
+++
@@ -1,9 +0,0 @@
-#%%
-import numpy as np 
-
-# %%
-exec(open('check_sector.py').read())
-# %%
-import plot_tess
-import check_sector
-# %%
